b47d886cb10caf100d489cad9d419d061e9b6e5a
Patrick Batey <patrickbatey@gmail.com>
2019-04-16 22:15:49 -0400

    updates

--- js/main.js
+++ js/main.js
@@ -5,18 +5,36 @@ $(document).ready(function(){
 function init() {
 	var log = console.log;
 	log('init');
+	
+	function isTouchDevice() {
+		return (typeof window.orientation !== "undefined") || (navigator.userAgent.indexOf('IEMobile') !== -1);
+	};
 
 	var t = TweenMax;
 	var hamburger = $('.hamburger');
+	
+	var _height = window.innerHeight;
+	
+	var ga_top = $('#getAirheaded').offset().top;
+	var fa_top = $('#floatAway').offset().top;
+	
+	var headerOffset = $('#header').height();
+	
+	log(headerOffset);
 
 	$(hamburger).click(function(){
 
 		if( $('.header').hasClass('open') ) {
 
-			$('.header').removeClass('open');
-			$('.navItem').removeClass('open');
-
-			//t.to($('.navItem'), 0.2, {opacity:0, ease:Power2.easeOut, onComplete:function(){ $('.navItem').removeClass('open'); } });
+			//$('.header').removeClass('open');
+			//$('.navItem').removeClass('open');
+			
+			t.to($('.navItem'), 0.1, {visibility:'hidden', opacity:0, ease:Power3.easeOut, onComplete:function(){
+				
+				$('.header').removeClass('open');
+				$('.navItem').removeClass('open');
+			
+			}});
 
 			t.to('#burger1', 0.3, {rotation:'0deg', y:'0px', ease:Power2.easeOut});
 			t.to('#burger2', 0.3, {opacity:1, ease:Power2.easeOut});
@@ -25,30 +43,40 @@ function init() {
 		} else {
 			$('.header').addClass('open');
 			$('.navItem').addClass('open');
-
-			//t.to($('.navItem'), 0.2, {opacity:1, ease:Power2.easeOut, onComplete:function(){ $('.navItem').addClass('open'); } });
+			
+			t.to($('.navItem'), 0.3, {visibility:'visible', opacity:1, ease:Power2.easeOut});
 
 			t.to('#burger1', 0.3, {rotation:'45deg', y:'9px', ease:Power2.easeOut});
 			t.to('#burger2', 0.3, {opacity:0, ease:Power2.easeOut});
 			t.to('#burger3', 0.3, {rotation:'-45deg',y:'-9px',  ease:Power2.easeOut});
 		}
 
-		log('ham click');
-
 	});
+	
+	if (!isTouchDevice()) {
+		$('.cta').mouseover(function(){
+			t.to( 	$(this), 0.2, {scale:1.1, ease:Power2.easeOut});
+		  	t.to( 	$('p', this), 0.2, {y:20, opacity:0, ease:Power3.easeOut});
+		  	t.set( 	$('p', this), {y:-20, delay:0.2});
+		  	t.to( 	$('p', this), 0.2, {y:0, opacity:1, ease:Power3.easeOut, delay:0.21});
+			
+		}).mouseout(function(){
+			t.to( 	$(this), 0.6, {scale:1.0, ease:Elastic.easeOut});
+		})
+	}
+	
+	
 
 	$('#cab_arrow').click(function(){
-		window.scrollTo({
-			top: 570,
-			behavior : 'smooth'
-		});
+		
+		t.to(window, 0.5, {scrollTo:fa_top - headerOffset, ease:Power2.easeOut});
+		
 	});
 
 	$('#fa_arrow').click(function(){
-		window.scrollTo({
-			top: 1140,
-			behavior : 'smooth'
-		});
+		
+		t.to(window, 0.5, {scrollTo:ga_top - headerOffset, ease:Power2.easeOut});
+		
 	});
 
 
