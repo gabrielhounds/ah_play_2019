133cedfa7ccd69a64e90722a9781787a5db00a2a
Patrick Batey <patrickbatey@gmail.com>
2019-04-17 14:17:13 -0400

    game pages

--- js/main.js
+++ js/main.js
@@ -4,25 +4,25 @@ $(document).ready(function(){
 function init() {
 	var log = console.log;
 	log('init');
-	
+
 	function isTouchDevice() {
 		return (typeof window.orientation !== "undefined") || (navigator.userAgent.indexOf('IEMobile') !== -1);
 	};
-	
+
 	if (isTouchDevice()) {
 		$('.game').css({height:window.innerHeight - headerOffset});
 	}
-	
+
 	var t = TweenMax;
-	
+
 	var hamburger = $('#hamburger');
-	
+
 	var _height = window.innerHeight;
-	
+
 	var ga_top = $('#getAirheaded').offset().top;
 	var fa_top = $('#floatAway').offset().top;
 	var headerOffset = $('#header').height();
-		
+
 	$(hamburger).click(function(){
 		if( $('.header').hasClass('open') ) {
 			//$('.header').removeClass('open');
@@ -43,7 +43,7 @@ function init() {
 			t.to('#burger3', 0.3, {rotation:'-45deg',y:'-9px',  ease:Power2.easeOut});
 		}
 	});
-	
+
 	if (!isTouchDevice()) {
 		$('.cta').mouseover(function(){
 			t.to( 	$(this), 0.2, {scale:1.1, ease:Power2.easeOut});
@@ -59,29 +59,29 @@ function init() {
 			t.to( $(this), 0.4, {backgroundPosition : 'center center', ease:Bounce.easeOut});
 		})
 	}
-	
+
 	$('#cab_arrow').click(function(){
 		t.to(window, 0.5, {scrollTo:fa_top - headerOffset, ease:Power2.easeOut});
 	});
 	$('#fa_arrow').click(function(){
 		t.to(window, 0.5, {scrollTo:ga_top - headerOffset, ease:Power2.easeOut});
 	});
-	
+
 	function reset() {
 		_height = window.innerHeight;
 		 ga_top = $('#getAirheaded').offset().top;
 		 fa_top = $('#floatAway').offset().top;
 		 headerOffset = $('#header').height();
-		 
+
 		 if (isTouchDevice()) {
 		 	$('.game').css({height:window.innerHeight - headerOffset});
 		}
 	}
-	
+
 	$( window ).resize(function() {
 		//reset();
 	});
-	
+
 	//window.addEventListener("orientationchange", reset() );
-	
+
 }
